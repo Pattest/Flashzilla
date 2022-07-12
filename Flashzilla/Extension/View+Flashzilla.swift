8646cd48204eadf6d2eac281fe422fd56f49cd6d
Pattest <baptistecadoux@gmail.com>
2022-07-12 09:59:42 +0200

    feat(Day 88): Project 17, part three
    - Designing a single card view
    - Building a stack of cards
    - Moving views with DragGesture and offset()

---
+++ Flashzilla/Extension/View+Flashzilla.swift
@@ -0,0 +1,15 @@
+//
+//  View+FZ.swift
+//  Flashzilla
+//
+//  Created by Baptiste Cadoux on 12/07/2022.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
